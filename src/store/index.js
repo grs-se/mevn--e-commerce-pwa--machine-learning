a59a6f7a7457e92c88f9793e709b413284cf18ed
grs-se <george_ricesmith@hotmail.com>
2022-12-31 19:50:29 +0000

    Add UserMovement and Authentication modules to index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,16 @@ import { createStore } from "vuex";
 import Products from "./modules/Products";
 import Categories from "./modules/Categories";
 import Cart from "./modules/Cart";
+import Authentication from "./modules/Authentication";
+import UserMovement from "./modules/UserMovement";
 
 const store = createStore({
 	modules: {
 		Products,
 		Categories,
 		Cart,
+		Authentication,
+		UserMovement,
 	},
 });
 
