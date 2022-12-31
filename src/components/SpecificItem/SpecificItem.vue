60845f7796c07a0a7a6ae7641e920deeff7e3137
grs-se <george_ricesmith@hotmail.com>
2022-12-31 12:03:51 +0000

    Use Cart inside SpecficItem component

--- src/components/SpecificItem/SpecificItem.vue
+++ src/components/SpecificItem/SpecificItem.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<center>
-			<div class="card">
+			<div v-if="isLoaded" class="card">
 				<img
 					class="card-img-top"
 					:src="ResultItems.imgSrc"
@@ -12,8 +12,18 @@
 					<p class="card-text">{{ ResultItems.desc }}</p>
 					<h4>Price : £{{ ResultItems.price }}</h4>
 					<h4><i class="fas fa-tags"></i> : {{ ResultItems.CatName }}</h4>
-					<a v-if="ResultItems.InCart" class="btn btn-primary"> Add To Card</a>
-					<a v-if="!ResultItems.InCart" class="btn btn-danger">
+					<a
+						v-if="!ResultItems.InCart"
+						class="btn btn-primary"
+						@click="ChangeItemStatus"
+					>
+						Add To Card</a
+					>
+					<a
+						v-if="ResultItems.InCart"
+						class="btn btn-danger"
+						@click="ChangeItemStatus"
+					>
 						Remove From Card</a
 					>
 				</div>
@@ -23,6 +33,7 @@
 </template>
 
 <script>
+// import { booleanLiteral } from "@babel/types";
 import { mapActions } from "vuex";
 
 export default {
@@ -38,6 +49,7 @@ export default {
 				CatName: "",
 				// InCart: true,
 			},
+			isLoaded: Boolean,
 		};
 	},
 	created() {
@@ -61,14 +73,23 @@ export default {
 		this.GetRouteData();
 	},
 	methods: {
-		...mapActions(["GetProdById", "GetCatById", "GetCategories"]),
+		...mapActions([
+			"GetProdById",
+			"GetCatById",
+			"GetCategories",
+			"CheckIfInCart",
+			"SetNewCartItem",
+			"RemoveItemFromCart",
+		]),
 
 		async GetRouteData() {
+			this.isLoaded = false;
+			//
 			this.GetCategories();
-			let ProdId = this.$route.query.ID;
-			await this.GetProdById(ProdId).then((res) => {
+			let ProdID = this.$route.query.ID;
+			await this.GetProdById(ProdID).then((res) => {
 				console.log("specific item", res);
-				this.ResultItems.id = ProdId;
+				this.ResultItems.id = ProdID;
 				this.ResultItems.name = res[0].name;
 				this.ResultItems.desc = res[0].desc;
 				this.ResultItems.price = res[0].price;
@@ -79,6 +100,25 @@ export default {
 					this.ResultItems.CatName = res[0].name;
 				});
 			});
+			// check if in cart
+			await this.CheckIfInCart(ProdID).then((res) => {
+				this.ResultItems.InCart = res;
+				console.log("res X", res);
+				this.isLoaded = true;
+			});
+		},
+		async ChangeItemStatus() {
+			this.isLoaded = false;
+			this.ResultItems.InCart = !this.ResultItems.InCart;
+			let ProdID = this.$route.query.ID;
+
+			if (!this.ResultItems.InCart) {
+				this.SetNewCartItem(ProdID);
+				this.isLoaded = true;
+			} else {
+				this.isLoaded = true;
+				this.RemoveItemFromCart(ProdID);
+			}
 		},
 	},
 };
