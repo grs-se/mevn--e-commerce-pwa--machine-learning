a4825c2922db3a10ef4842d73dfea23fac9eb652
grs-se <george_ricesmith@hotmail.com>
2023-01-02 19:23:18 +0000

    Implement Search functiionality and component

---
+++ src/components/Header/Search.vue
@@ -0,0 +1,42 @@
+<template>
+	<i>
+		<input
+			v-model="SearchData"
+			class="form-control mr-sm-0"
+			type="search"
+			placeholder="Search"
+			aria-label="Search"
+		/>
+		<button
+			class="btn btn-outline-success my-2 my-sm-2"
+			@click="Search($event)"
+		>
+			Search
+		</button>
+	</i>
+</template>
+
+<script>
+export default {
+	name: "SearchView",
+	data() {
+		return {
+			SearchData: "",
+		};
+	},
+	methods: {
+		Search(e) {
+			e.preventDefault();
+			this.$router
+				.push({
+					path: "/SearchProduct",
+					name: "SearchProduct",
+					query: {
+						SData: this.SearchData,
+					},
+				})
+				.catch(() => {});
+		},
+	},
+};
+</script>
---
+++ src/components/SearchProduct/SearchProduct.vue
@@ -0,0 +1,52 @@
+<template>
+	<center>
+		<div>
+			<h3>
+				Searching for :
+				<small>{{ SearchData }}</small>
+			</h3>
+
+			<div class="col-md-9">
+				<div class="container">
+					<div class="card-group">
+						<div class="row">
+							<div class="card col-md-4">
+								<img
+									class="card-img-top"
+									src="https://priceintanzania.com/wp-content/uploads/2020/09/Huawei-Enjoy-20-5G.jpg"
+									alt="Card image cap"
+								/>
+								<div class="card-body">
+									<h6>Price: $1000</h6>
+									<h5 class="card-title">Card title</h5>
+									<button class="btn btn-danger">
+										<small> Add to cart</small>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</center>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			SearchData: "",
+		};
+	},
+	watch: {
+		$route(to, from) {
+			this.SearchData = to.query.SData;
+			from;
+		},
+	},
+	mounted() {
+		this.SearchData = this.$route.query.SData;
+	},
+};
+</script>
