5b11e5ddbc1e3aec8b129b5cb1e97f716b29022e
grs-se <george_ricesmith@hotmail.com>
2023-01-03 21:02:25 +0000

    Add actions to Categories store module

--- src/store/modules/Categories.js
+++ src/store/modules/Categories.js
@@ -25,10 +25,43 @@ const actions = {
 		commit("non");
 		return item;
 	},
+	async ADD_NEW_CAT({ commit }, data) {
+		let NewDataArr = {};
+		let newID = Math.floor(Math.random() * 50) + 10;
+		NewDataArr = { id: newID, name: data.NewCatName };
+		commit("NewCategories", NewDataArr);
+	},
+	async EditOneCat({ commit }, data) {
+		let objIndex = state.CategoriesList.findIndex((obj) => obj.id === data.ID);
+		let name = data.name;
+		let newDataObj = { objIndex, name };
+		commit("EditOneCategory", newDataObj);
+	},
+	async DeleteOneCat({ commit }, data) {
+		let newArrDel = state.CategoriesList.filter((x) => {
+			return x.id != data.ID;
+		});
+		commit("ResetAndDelete", newArrDel);
+	},
+	async GetCatByPageNum({ commit }) {
+		const newCatData = [
+			{ id: 77, name: "phone P" },
+			{ id: 88, name: "laptop P" },
+			{ id: 99, name: "cameras P" },
+			{ id: 100, name: "shirts P" },
+			{ id: 101, name: "shoes P" },
+			{ id: 102, name: "pants P" },
+		];
+		commit("GetCategories", newCatData);
+	},
 };
 
 const mutations = {
-	GetCategories: (state, categories) => (state.CategoriesList = categories),
+	SetCategories: (state, categories) => (state.CategoriesList = categories),
+	NewCategories: (state, newData) => state.CategoriesList.unshift(newData),
+	EditOneCategories: (state, NewDataObj) =>
+		(state.CategoriesList[NewDataObj.objIndex].name = NewDataObj.name),
+	ResetAndDelete: (state, newArrDel) => (state.CategoriesList = newArrDel),
 	non: (state) => (state.non = ""),
 };
 
