1458936eaaabaf07fa25e9f586dfd242dbb78f39
grs-se <george_ricesmith@hotmail.com>
2023-01-31 10:20:08 +0000

    LoginRegister

--- src/components/LoginRegister/LoginRegister.vue
+++ src/components/LoginRegister/LoginRegister.vue
@@ -13,6 +13,13 @@
 							</div>
 							<form>
 								<div class="row">
+									<div
+										v-if="RegisterError !== ''"
+										class="alert alert-danger"
+										role="alert"
+									>
+										{{ RegisterError }}
+									</div>
 									<div class="col-md-6">
 										<div class="form-group">
 											<input
@@ -125,6 +132,13 @@
 							</div>
 							<form>
 								<div class="row">
+									<div
+										v-if="LoginError"
+										class="alert alert-danger"
+										role="alert"
+									>
+										Invalid username or password
+									</div>
 									<div class="col-md-6">
 										<div class="form-group">
 											<input
@@ -162,42 +176,119 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from 'vuex';
+import axios from 'axios';
+const port = process.env.PORT || 3000;
+const URL_backend = `http://localhost:${port}/api`;
+
 export default {
-	name: "LoginRegister",
+	name: 'LoginRegister',
 	data() {
 		return {
 			RegisterData: {
-				FName: "",
-				LName: "",
-				Email: "",
+				FName: '',
+				LName: '',
+				Email: '',
 				GenderMale: false,
 				GenderFemale: false,
-				Pass: "",
-				confirmPass: "",
+				Pass: '',
+				confirmPass: '',
 				UserDate: [],
 			},
 			LoginData: {
-				Email: "",
-				Password: "",
+				Email: '',
+				Password: '',
 			},
+			LoginError: false,
+			RegisterError: '',
 		};
 	},
+	computed: {
+		...mapGetters(['UserAuth']),
+	},
 	methods: {
-		...mapActions(["SetUserAuth"]),
+		...mapActions(['SetUserAuth']),
 		Register: function (e) {
 			e.preventDefault();
-			console.log("reg", this.RegisterData);
-			let data = { isLoggedIn: true, isAdmin: true };
-			this.SetUserAuth(data);
-			this.$router.push({ path: "/UserProfile" });
+			let userData;
+			// check if user male or female
+			if (this.RegisterData.GenderMale == 'true') {
+				this.RegisterData.GenderMale = false;
+				userData = {
+					first_name: String(this.RegisterData.FName),
+					last_name: String(this.RegisterData.LName),
+					email: String(this.RegisterData.Email),
+					password: String(this.RegisterData.confirmPass),
+					gender: 'male',
+					birth_day: String(this.RegisterData.UserDate),
+				};
+			} else {
+				this.RegisterData.GenderFemale = false;
+				userData = {
+					first_name: String(this.RegisterData.FName),
+					last_name: String(this.RegisterData.LName),
+					email: String(this.RegisterData.Email),
+					password: String(this.RegisterData.confirmPass),
+					gender: 'female',
+					birth_day: String(this.RegisterData.UserDate),
+				};
+			}
+
+			// check if password is matched
+			if (this.RegisterData.Pass !== this.RegisterData.confirmPass) {
+				this.RegisterError = 'Password not matched';
+				this.RegisterData.Pass = '';
+				this.RegisterData.confirmPass = '';
+			} else {
+				axios
+					.post(`${URL_backend}/users`, userData)
+					.then((res) => {
+						console.log('Register res', res);
+						this.LoginData.Email = res.data.email;
+						this.LoginData.Password = this.RegisterData.confirmPass;
+						this.RegisterError = '';
+					})
+					.catch((err) => (this.RegisterError = err.response.data));
+			}
+			// console.log('reg', this.RegisterData);
+			// let data = { isLoggedIn: true, isAdmin: true };
+			// this.SetUserAuth(data);
+			// this.$router.push({ path: '/UserProfile' });
 		},
 		Login: function (e) {
 			e.preventDefault();
-			console.log("Log", this.LoginData);
-			let data = { isLoggedIn: true, isAdmin: true };
-			this.SetUserAuth(data);
-			this.$router.push({ path: "/UserProfile" });
+			console.log('Log', this.LoginData);
+			// if operation is successful
+			let userData = {
+				email: String(this.LoginData.Email),
+				password: String(this.LoginData.Password),
+			};
+			axios
+				.post(`${URL_backend}/auth`, userData)
+				.then((res) => {
+					getUserData(res);
+					this.LoginError = false;
+				})
+				.catch((err) => {
+					this.LoginError = true;
+					console.log('err', err.response.data.msg);
+				});
+
+			const getUserData = (Token) => {
+				axios
+					.get(`${URL_backend}/users/UserData`, {
+						headers: { 'x-auth-token': String(Token.data) },
+					})
+					.then((res) => {
+						let data = {
+							isLoggedIn: true,
+							isAdmin: res.data.isAdmin,
+							Token: String(Token.data),
+						};
+						this.SetUserAuth(data);
+						this.$router.push({ path: '/UserProfile' });
+					});
+			};
 		},
 	},
 };
