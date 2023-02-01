f67e10c032e830ac0928fc3ef0e0be8cfe1b81b0
grs-se <george_ricesmith@hotmail.com>
2023-02-01 21:32:46 +0000

    App-backend integrationTest User Middlware

--- backend/index.js
+++ backend/index.js
@@ -20,4 +20,8 @@ require('./startup/db')();
 //
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+	winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
---
+++ backend/tests/integration/auth.test.js
@@ -0,0 +1,51 @@
+const { User } = require('../../models/users');
+const { Categories } = require('../../models/categories');
+
+const request = require('supertest');
+let server = require('../../index');
+
+describe('auth middleware', () => {
+	beforeEach(() => {
+		// server = require('../../index');
+	});
+
+	afterEach(async () => {
+		await Categories.remove({});
+		server.close;
+	});
+
+	let token;
+
+	const exec = () => {
+		return request(server)
+			.post('/api/categories')
+			.set('x-auth-token', token)
+			.send({ name: 'newCatTest' });
+	};
+
+	beforeEach(() => {
+		token = new User({ isAdmin: true }).generateAuthToken();
+	});
+
+	it('should return 401 if no token is provided', async () => {
+		token = '';
+
+		const res = await exec();
+
+		expect(res.status).toBe(401);
+	});
+
+	it('should return 400 if token is invalid', async () => {
+		token = 'a';
+
+		const res = await exec();
+
+		expect(res.status).toBe(400);
+	});
+
+	it('should return 200 if token is valid and user is Admin', async () => {
+		const res = await exec();
+
+		expect(res.status).toBe(200);
+	});
+});
